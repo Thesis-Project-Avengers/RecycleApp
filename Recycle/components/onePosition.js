ffc847970303138df81c77d7a5f0cd25a53ab75a
Jesserbenkhiria <jesserbenkhiria911@gmail.com>
2023-10-31 23:08:46 +0100

    map modal styling

--- Recycle/components/onePosition.js
+++ Recycle/components/onePosition.js
@@ -2,22 +2,41 @@ import { View, Text } from "react-native";
 import React from "react";
 import { Marker } from "react-native-maps";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
-const OnePosition = ({loc,setselectedPos,setVisibleModal,getSelectedInformation}) => {
-    
+const OnePosition = ({
+  loc,
+  setselectedPos,
+  setVisibleModal,
+  getSelectedInformation,
+}) => {
   return (
     <Marker
-    //   onPress={setselectedPos({
-    //     latitude: loc.location.latitude,
-    //     longitude: loc.location.longitude,
-    //   })}
-      onPress={()=>{setselectedPos(loc);setVisibleModal(1);getSelectedInformation()}}
+      //   onPress={setselectedPos({
+      //     latitude: loc.location.latitude,
+      //     longitude: loc.location.longitude,
+      //   })}
+      onPress={() => {
+        setselectedPos(loc);
+        setVisibleModal(1);
+        getSelectedInformation(loc);
+      }}
       coordinate={{
         latitude: loc.location.latitude,
         longitude: loc.location.longitude,
       }}
       pinColor={"black"}
     >
-      <Icon name="bottle-soda" size={40} color={"#73d905"} />
+      <Icon
+        name="bottle-soda"
+        size={30}
+        color={"#93C572"}
+        style={{
+          backgroundColor: "#186F65",
+          borderRadius: 50,
+          borderWidth: 1,
+          borderStyle: "solid",
+          borderColor: "#93C572",
+        }}
+      />
     </Marker>
   );
 };
