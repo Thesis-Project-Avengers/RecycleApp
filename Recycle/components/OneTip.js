ec07b02d68bfbe6c0de364c93acf1522fca2a739
Blhasn-Sehli <sehlibellhassen@gmail.com>
2023-11-01 15:36:27 +0100

    done

--- Recycle/components/OneTip.js
+++ Recycle/components/OneTip.js
@@ -5,7 +5,7 @@ import React from 'react'
 
 const OneTip = () => {
     return (
-        <View style={{marginBottom:20,borderWidth: 2, borderColor: "#eee", borderRadius: 20, height: 350, width: "100%", gap: 10, flexDirection: 'column', padding: 15 }} >
+        <View style={{marginBottom:20,borderWidth: 2, borderColor: "#eee", borderRadius: 20, height: 350, width: "100%", gap: 10, flexDirection: 'column', pa: 20 }} >
             <View style={{ flexDirection: "row", alignItems: 'center', justifyContent: "space-between" }}>
                 <View style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
                     <Image borderRadius={50} source={{ uri: "https://imgs.search.brave.com/GTJCGfegRVtsmO_8q1JhKHaKmJ6Fh3hcEwzK6m3klog/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zcC1h/by5zaG9ydHBpeGVs/LmFpL2NsaWVudC90/b193ZWJwLHFfZ2xv/c3N5LHJldF9pbWcs/d18zMDAsaF8zMDAv/aHR0cHM6Ly9ibG9n/LnNuYXBwYS5jb20v/d3AtY29udGVudC91/cGxvYWRzLzIwMjEv/MDgvZG90dGVkLWJh/Y2tncm91bmQtYXZh/dGFyLWV4YW1wbGUt/MzAweDMwMC5qcGc" }} width={50} height={50} />
