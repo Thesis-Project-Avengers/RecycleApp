c8d6e8f13eda6fd59cdbf57e891f71d3084df9dd
khalil-kouroghli <mohamedkhalil.kouroghli@gmail.com>
2023-11-04 10:06:55 +0100

    khalil

--- Recycle/App.js
+++ Recycle/App.js
@@ -53,7 +53,7 @@ export default function App() {
   }, [])
   return (
     <NavigationContainer >
-      <Stack.Navigator initialRouteName={"ombording"} screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName={"App"} screenOptions={{ headerShown: false }}>
         <Stack.Screen name="ombording" component={OnboardingScreen} />
         <Stack.Screen name="auth" component={AuthStack} />
         <Stack.Screen name="App" component={RealApp} />
@@ -63,7 +63,7 @@ export default function App() {
 }
 export const AuthStack = () => {
   return (
-    <Stack.Navigator initialRouteName="signIn" screenOptions={{ headerShown: false }}>
+    <Stack.Navigator initialRouteName="profileCollector" screenOptions={{ headerShown: false }}>
       <Stack.Screen name="signIn" component={SignIn} />
       <Stack.Screen name="confirmCode" component={ConfirmCode} />
       <Stack.Screen name="formAfterAuth" component={FormAfterAuth} />
