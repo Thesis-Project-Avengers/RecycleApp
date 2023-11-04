cd78612b58fbc866ef7c12f8d46ac5443dbcf8f0
khalil-kouroghli <mohamedkhalil.kouroghli@gmail.com>
2023-11-04 12:50:54 +0100

    profile 70%

--- Recycle/screens/Profile.js
+++ Recycle/screens/Profile.js
@@ -1,48 +1,148 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { SafeAreaView, StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from "react-native";
+import React from "react";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import Icon2 from "react-native-vector-icons/FontAwesome5";
+import Icon3 from "react-native-vector-icons/FontAwesome5";
+import Icon4 from "react-native-vector-icons/Entypo";
+import Icon5 from "react-native-vector-icons/AntDesign";
 
 const Profile = () => {
   return (
-    <SafeAreaView style={{padding:"40%"}}>
-        <View style={styles.returnPoints} >
-        <View style={styles.return} >
-            <Text style={{textAlign:"center"}}>return</Text>
-          </View>
-          <View style={styles.points} >
-            <Text style={{textAlign:"center",color:"white",fontSize:"16"}}>points</Text>
-          </View>
-        </View>  
+    <SafeAreaView  style={{ padding: "40%" }}>
+    <ScrollView showsVerticalScrollIndicator={false} style={styles.scroll} >
+      <View style={styles.returnPoints}>
+        <View style={styles.return}>
+          {/* <Icon name="arrow-return-left" 
+          size={22}
+          color={"#93C572"}
 
-        <View style={styles.imageTextName} >
-        
-
+          /> */}
         </View>
-
-
+        <View style={styles.points}>
+          <Text style={{ textAlign: "center", color: "white", fontSize: "16" }}>
+            points
+          </Text>
+        </View>
+      </View>
+      <View style={styles.imageTextName}>
+        <Image
+          source={require("../assets/khalil.jpg")}
+          style={styles.imageProfile}
+        />
+        <Text style={styles.textName}>khalil kouroghli</Text>
+      </View>
+      <View style={styles.statContainer}>
+        <View style={styles.oneRec}>
+          <Icon name="bottle-wine-outline" size={40} color={"#93C572"} />
+          <Text>20</Text>
+        </View>
+        <View style={styles.oneRec}>
+          <Icon2
+            name="people-arrows" size={40} color={"#93C572"}
+          />
+          <Text>35</Text>
+        </View>
+        <View style={styles.oneRec}>
+          <Icon3
+            name="money-bill-wave" size={40} color={"#93C572"}
+          />
+          <Text>100</Text>
+        </View>
+      </View>
+        {/* <ScrollView style={styles.scroll}> */}
+          <View style={{gap:10}}>
+          <View style={styles.oneButton}>
+          <Icon4 name="back-in-time" size={40} color={"#93C572"} />
+          <TouchableOpacity><Text style={{fontSize:17}}>My Payments</Text></TouchableOpacity>
+          </View>
+          <View  style={styles.oneButton}>
+            <Icon5 name="qrcode" size={40} color={"#93C572"} />
+          <TouchableOpacity><Text style={{fontSize:23}}>My Qr Code</Text></TouchableOpacity>
+          </View>
+          <View  style={styles.oneButton}>
+          <Icon5 name="edit" size={40} color={"#93C572"}/>
+          <TouchableOpacity><Text style={{fontSize:23}}>Edit Profile</Text></TouchableOpacity>
+          </View>
+          <View  style={styles.oneButton}>
+          <Icon name="logout" size={40} color={"#93C572"} />
+          <TouchableOpacity><Text style={{fontSize:23}}>Log Out</Text></TouchableOpacity>
+          </View>
+          <View  style={styles.oneButton}>
+          <Icon name="logout" size={40} color={"#93C572"} />
+          <TouchableOpacity><Text style={{fontSize:23}}>Delete Account</Text></TouchableOpacity>
+          </View>
+          </View>
+    </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
 
 const styles = StyleSheet.create({
-  returnPoints:{
-    flexDirection:"row", 
-    justifyContent:"space-between",
-    // backgroundColor:"red", 
-    padding:20,
-    alignContent:"center"
+  returnPoints: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    // backgroundColor:"red",
+    padding: 5,
+    alignContent: "center",
   },
-points:{
-backgroundColor:"#93C572",
-borderRadius:20,
-padding:15,
-width:90,
-textAlign:"center"
+  points: {
+    backgroundColor: "#93C572",
+    borderRadius: 20,
+    padding: 15,
+    width: 90,
+    textAlign: "center",
   },
-  return:{
-    padding:15,
-width:90,
-textAlign:"center"
+  return: {
+    padding: 15,
+    width: 90,
+    textAlign: "center",
+  },
+  imageTextName: {
+    // backgroundColor:"red",
+    gap: 10,
+  },
+  imageProfile: {
+    alignSelf: "center",
+    borderRadius: 50,
+    width: 100,
+    height: 100,
+  },
+  textName: {
+    alignSelf: "center",
+    fontSize: "18",
+  },
+  statContainer: {
+    flexDirection: "row",
+    // backgroundColor: "red",
+    padding: 10,
+    marginTop: 10,
+    justifyContent: "space-around",
+    height: "20%",
+    alignItems: "center",
+  },
+  oneRec: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    // backgroundColor: "green",
+    padding: 15,
+    gap:10, 
+height:"100%"
+  },
+  scroll:{
+    padding:5,
+    height:"100%"
+    // backgroundColor:"red",
+    // gap:30
+  },
+  oneButton:{
+    // backgroundColor:"green",
+    width:"100%", 
+    padding:30,
+    flexDirection:"row",
+    alignItems:"center",
+    gap:25
   }
-})
+});
