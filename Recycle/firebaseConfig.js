947be1fc5dadd6a4605d03ffc0a6e2559ff1d1dd
Blhasn-Sehli <sehlibellhassen@gmail.com>
2023-11-20 18:46:16 +0100

    fixed

--- Recycle/firebaseConfig.js
+++ Recycle/firebaseConfig.js
@@ -10,15 +10,25 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-export const firebaseConfig = {
-  apiKey: "AIzaSyDIX9agEqxrTUPV7qydY49Ja40WJ3z_SIg",
-  authDomain: "recycle-20fae.firebaseapp.com",
-  projectId: "recycle-20fae",
-  databaseURL: 'https://recycle-20fae-default-rtdb.europe-west1.firebasedatabase.app',
-  storageBucket: "recycle-20fae.appspot.com",
-  messagingSenderId: "637778408728",
-  appId: "1:637778408728:web:cb36d1547108f4cd659a6d"
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyDIX9agEqxrTUPV7qydY49Ja40WJ3z_SIg",
+//   authDomain: "recycle-20fae.firebaseapp.com",
+//   projectId: "recycle-20fae",
+//   databaseURL: 'https://recycle-20fae-default-rtdb.europe-west1.firebasedatabase.app',
+//   storageBucket: "recycle-20fae.appspot.com",
+//   messagingSenderId: "637778408728",
+//   appId: "1:637778408728:web:cb36d1547108f4cd659a6d"
+// };
+const firebaseConfig = {
+  apiKey: "AIzaSyDaiKBV81jX2kz8GWWFV-gJRp_sy6R_pfc",
+  authDomain: "recyclev2-44dd8.firebaseapp.com",
+  projectId: "recyclev2-44dd8",
+  storageBucket: "recyclev2-44dd8.appspot.com",
+  messagingSenderId: "358013929699",
+  appId: "1:358013929699:web:68de6fa51703aeefed5dd6"
 };
+
+
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
