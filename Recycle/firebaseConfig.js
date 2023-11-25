c2c770f5ea7cce022398620433f3811a75ac1cea
Blhasn-Sehli <sehlibellhassen@gmail.com>
2023-11-25 20:52:59 +0100

    adding the new Config For the Firebase

--- Recycle/firebaseConfig.js
+++ Recycle/firebaseConfig.js
@@ -9,27 +9,16 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// export const firebaseConfig = {
-//   apiKey: "AIzaSyDIX9agEqxrTUPV7qydY49Ja40WJ3z_SIg",
-//   authDomain: "recycle-20fae.firebaseapp.com",
-//   projectId: "recycle-20fae",
-//   databaseURL: 'https://recycle-20fae-default-rtdb.europe-west1.firebasedatabase.app',
-//   storageBucket: "recycle-20fae.appspot.com",
-//   messagingSenderId: "637778408728",
-//   appId: "1:637778408728:web:cb36d1547108f4cd659a6d"
-// };
+
 export const firebaseConfig = {
-  apiKey: "AIzaSyDaiKBV81jX2kz8GWWFV-gJRp_sy6R_pfc",
-  authDomain: "recyclev2-44dd8.firebaseapp.com",
-  projectId: "recyclev2-44dd8",
-  databaseURL: "https://recyclev2-44dd8-default-rtdb.europe-west1.firebasedatabase.app",
-  storageBucket: "recyclev2-44dd8.appspot.com",
-  messagingSenderId: "358013929699",
-  appId: "1:358013929699:web:68de6fa51703aeefed5dd6"
+  apiKey: "AIzaSyCKXjGQCvv58uhAEhCZ6e6A5-6hKpUqYiI",
+  authDomain: "recyclev3-6f26a.firebaseapp.com",
+  projectId: "recyclev3-6f26a",
+  databaseURL: "https://recyclev3-6f26a-default-rtdb.europe-west1.firebasedatabase.app",
+  storageBucket: "recyclev3-6f26a.appspot.com",
+  messagingSenderId: "472368470135",
+  appId: "1:472368470135:web:2c17eaa67817803431ce72"
 };
-
-
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
