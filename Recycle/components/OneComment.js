e025f00398c830a2ed7f10fe671f734c5cece06f
Blhasn-Sehli <sehlibellhassen@gmail.com>
2023-11-28 18:48:35 +0100

    Comment User Fixed In The Back

--- Recycle/components/OneComment.js
+++ Recycle/components/OneComment.js
@@ -1,14 +1,25 @@
 import { View, Text, StyleSheet, Image } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { useFocusEffect } from '@react-navigation/native'
+import { useCallback } from 'react'
+import { doc, getDoc } from 'firebase/firestore'
+import { FIREBASE_DB } from '../firebaseConfig'
 
 const OneComment = ({ comment }) => {
+    const [commenterInfo,setCommenterInfo] = useState(null)
+    useFocusEffect(useCallback(()=>{
+        const commenterDocRef=doc(FIREBASE_DB,"users",comment?.commenterId);
+        getDoc(commenterDocRef).then((doc) => {
+            setCommenterInfo(doc.data())
+        })
+    },[]))
     return (
         <View style={styles.container}>
             <View style={styles.commentHeader}>
                 <View style={{ flexDirection: 'row', alignItems: "center", gap: 10 }}>
-                    <Image style={{ borderColor: "#93C572", borderWidth: 2 }} source={{ uri: comment?.user?.photoURL }} width={40} height={40} borderRadius={50} />
+                    <Image style={{ borderColor: "#93C572", borderWidth: 2 }} source={{ uri: commenterInfo?.photoURL }} width={40} height={40} borderRadius={50} />
                     <Text>
-                        {comment?.user.displayName}
+                        {commenterInfo?.displayName}
                     </Text>
                 </View>
                 <Text >
